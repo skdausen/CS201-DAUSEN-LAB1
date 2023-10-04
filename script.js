e618197ded96d3b39a6ace5443b311289a2db91c
Sean Kelly Dausen <146722016+skdausen@users.noreply.github.com>
2023-10-04 14:10:33 +0800

    feat: Activity 1 in DSA
    Basic Math using javaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+// Activity 1
+
+// Declaration of variables
+let firstFavNum = 10;
+let secondFavNum = 8;
+
+console.log('ACTIVITY 1');
+console.log(firstFavNum + secondFavNum);    // add
+console.log(firstFavNum - secondFavNum);    // subtract
+console.log(firstFavNum * secondFavNum);    // multiply
+console.log(firstFavNum / secondFavNum);    // divide
+console.log(firstFavNum % secondFavNum);    // check modulo or remainder
+console.log(Math.pow(firstFavNum));         // check the power of firstFavNum
